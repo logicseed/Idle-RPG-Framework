246dde6415a7d8b956e17283154628141aed2251
Marc King <mjking@umich.edu>
2017-04-01 13:50:30 -0400

    First interation of creating stub classes.

---
+++ Assets/Scripts/Controllers/HazardController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardController : PlaceableBehaviour {
+
+    public HazardType type;
+    public float potency;
+    public bool isOneTime;
+    public bool isPathable;
+
+    [HideInInspector]
+    public bool hasTriggered;
+
+    public void ApplyEffect()
+    {
+
+    }
+
+    public void OnTriggerEnter2D()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Managers/QueueManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QueueManager : MonoBehaviour
+{
+    public List<Enemy> enemies;
+    public float timeBetweenSpawn;
+    public bool repeating;
+
+	public IEnumerator SpawnNext()
+    {
+
+    }
+
+
+}
