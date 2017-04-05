e7593241b16be5d2afa6f0736ebc0acb52e7be23
Marc King <mjking@umich.edu>
2017-04-05 15:55:28 -0400

    Started testing scene for movement behaviours.

--- Assets/Scripts/Movement/AvoidMovementBehaviour.cs
+++ Assets/Scripts/Movement/AvoidMovementBehaviour.cs
@@ -6,15 +6,15 @@
 /// </summary>
 public class AvoidMovementBehaviour : DirectMovementBehaviour
 {
-
+    private float avoidRadius;
     /// <summary>
     /// Constructor for AvoidMovementBehaviour instances.
     /// </summary>
     /// <param name="movementBehaviour">The movement behaviour to decorate.</param>
     /// <param name="agent">The GameObject that desires this movement behaviour.</param>
     /// <param name="target">The target of this movement behaviour.</param>
-    public AvoidMovementBehaviour(AbstractMovementBehaviour movementBehaviour, GameObject agent, GameObject target)
-         : base(movementBehaviour, agent, target) { }
+    public AvoidMovementBehaviour(AbstractMovementBehaviour movementBehaviour, GameObject agent, GameObject target, float avoidRadius)
+         : base(movementBehaviour, agent, target) { this.avoidRadius = avoidRadius; }
 
     /// <summary>
     /// The steering vector desired by this movement behaviour.
@@ -22,7 +22,12 @@ public class AvoidMovementBehaviour : DirectMovementBehaviour
     /// <returns>The optimal steering vector to accomplish this movement behaviour.</returns>
     public override Vector2 Steering()
     {
-        var desiredSteering = CalculateDesiredSteering(target.transform.position, agent.transform.position);
+        var desiredSteering = Vector2.zero;
+
+        if (Vector2.Distance(agent.transform.position, target.transform.position) > avoidRadius)
+        {
+            desiredSteering = CalculateDesiredSteering(target.transform.position, agent.transform.position);
+        }
 
         return desiredSteering + movementBehaviour.Steering();
     }
---
+++ Assets/Tests/MovementBehaviourTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviourTests : MonoBehaviour {
+
+    public GameObject seekTarget;
+    public GameObject avoidTarget;
+    private AbstractMovementBehaviour movementBehaviour;
+    private MovementController controller;
+
+	// Use this for initialization
+	void Start () {
+        movementBehaviour = new IdleMovementBehaviour();
+        movementBehaviour = new SeekMovementBehaviour(movementBehaviour, gameObject, seekTarget);
+        movementBehaviour = new AvoidMovementBehaviour(movementBehaviour, gameObject, avoidTarget, 0.5f);
+        controller = gameObject.GetComponent<MovementController>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var newPosition = movementBehaviour.Steering().normalized * controller.maxSpeed;
+        transform.Translate(newPosition);
+	}
+}
