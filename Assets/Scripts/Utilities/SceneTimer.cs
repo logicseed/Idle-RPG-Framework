f7f749d230f521bbc5fd432c019288b30230c8d0
Marc King <mjking@umich.edu>
2017-04-15 14:09:24 -0400

    Testing stage background, initial work on zone/stage selection.

---
+++ Assets/Scripts/Utilities/SceneTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Loads a scene after a specified amount of time.
+/// </summary>
+public class SceneTimer : MonoBehaviour
+{
+    public string scene;
+    public float delay;
+
+	void Start ()
+    {
+        StartCoroutine("DelayLoad");
+    }
+
+    IEnumerator DelayLoad()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(scene);
+    }
+}
