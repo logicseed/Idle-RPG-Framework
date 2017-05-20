0a7c9e6fada5e080d7dab0cc43e4e39f6c4df8f4
adashti <adashti@UMD-CIS239LAB10>
2017-05-20 14:05:37 -0400

    Started the "Animation Movement" in the MovementBehaviourTests.unity scene. Only Walking and Idling done so far.

---
+++ Assets/Scripts/AnimationBehaviour.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBehaviour : MonoBehaviour 
+{
+	/*
+	 * Animation States:
+	 * 0 : Idle Left
+	 * 1 : Idle Right
+     * 2 : Walk Left
+     * 3 : Walk Right
+	 */
+
+	Animator anim;
+    Vector2 vector;
+	int state;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+        state = -1;
+        anim.SetInteger("state", state);
+        vector = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animationMovement();
+    }
+
+    void animationMovement()
+    {
+        //If player WAS moving to the right, Idle right
+        if (vector.x == this.transform.position.x
+            && state == 3)
+        {
+            state = 1;
+            anim.SetInteger("state", state);
+
+            //Update current position
+            vector = this.transform.position;
+        }
+
+        //If player WAS moving to the left, Idle left
+        if (vector.x == this.transform.position.x
+            && state == 2)
+        {
+            state = 0;
+            anim.SetInteger("state", state);
+
+            //Update current position
+            vector = this.transform.position;
+        }
+
+        //If player is walking right
+        if (vector.x < this.transform.position.x)
+        {
+            state = 3;
+            anim.SetInteger("state", state);
+
+            //Update current position
+            vector = this.transform.position;
+        }
+
+        //If player is walking left
+        if (vector.x > this.transform.position.x)
+        {
+            state = 2;
+            anim.SetInteger("state", state);
+
+            //Update current position
+            vector = this.transform.position;
+        }
+    }
+}
