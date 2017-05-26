cf8561431bdab8ac704a33764bd22ab5bc6dadcf
Marc King <mjking@umich.edu>
2017-05-25 22:27:30 -0400

    Added custom help attribute for displaying help in the inspector.

---
+++ Assets/Editor/HelpBoxAttributeDrawer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(HelpBoxAttribute))]
+public class HelpBoxAttributeDrawer : DecoratorDrawer
+{
+    public override void OnGUI(Rect position)
+    {
+        var helpBoxAttribute = attribute as HelpBoxAttribute;
+
+        if (helpBoxAttribute == null) return;
+
+        EditorGUI.HelpBox(position, helpBoxAttribute.message, GetMessageType(helpBoxAttribute.messageType));
+    }
+
+    public override float GetHeight()
+    {
+        var helpBoxAttribute = attribute as HelpBoxAttribute;
+        if (helpBoxAttribute == null) return base.GetHeight();
+
+        var helpBoxStyle = (GUI.skin != null) ? GUI.skin.GetStyle("helpbox") : null;
+        if (helpBoxStyle == null) return base.GetHeight();
+
+        return Mathf.Max(30f, helpBoxStyle.CalcHeight(new GUIContent(helpBoxAttribute.message), EditorGUIUtility.currentViewWidth) + 5);
+    }
+
+
+    private MessageType GetMessageType(HelpBoxMessageType helpBoxMessageType)
+    {
+        return (MessageType)helpBoxMessageType;
+    }
+}
