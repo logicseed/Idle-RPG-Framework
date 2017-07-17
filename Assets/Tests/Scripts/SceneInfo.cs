ce0c276c3c19d35ed22ab7e93cbd69ddf670c649
Marc King <marc.king@gmail.com>
2017-07-17 16:42:18 -0400

    Dynamic ally spawning integration test scene.

---
+++ Assets/Tests/Scripts/SceneInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class SceneInfo : MonoBehaviour
+{
+    public Text displayText;
+	
+	void Update ()
+    {
+        var frameText = new StringBuilder();
+        frameText.AppendLine("SCENE INFO");
+        frameText.AppendLine("Time: " + Time.timeSinceLevelLoad);
+
+        displayText.text = frameText.ToString();
+	}
+}
