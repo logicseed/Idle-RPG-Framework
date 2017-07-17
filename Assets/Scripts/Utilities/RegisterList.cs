3832530b7e6b078df7ce11b683e3735a05028867
Marc King <marc.king@gmail.com>
2017-07-17 16:38:50 -0400

    Created generic register list for the game manager to use to store stage entity references.

---
+++ Assets/Scripts/Utilities/RegisterList.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class RegisterList<T> where T : MonoBehaviour
+{
+    [SerializeField]
+    protected List<T> list;
+
+    public RegisterList()
+    {
+        RemoveAll();
+    }
+
+    public virtual List<T> GetAll()
+    {
+        return list;
+    }
+
+    public virtual void RemoveAll(bool destroy = false)
+    {
+        if (destroy)
+        {
+            foreach (var item in list)
+            {
+                GameObject.Destroy(item.gameObject);
+            }
+        }
+
+        list = new List<T>();
+    }
+
+    public virtual void Register(T item)
+    {
+        if (!list.Contains(item)) list.Add(item);
+    }
+
+    public virtual void Unregister(T item)
+    {
+        if (list.Contains(item)) list.Remove(item);
+    }
+
+    public virtual void AddAllToList(ref List<T> addToList)
+    {
+        if (list.Count > 0) addToList.AddRange(list);
+    }
+}
