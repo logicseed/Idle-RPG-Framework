87aaf5b3f87f4be5808c649d6144d34050203550
Marc King <marc.king@gmail.com>
2017-07-17 16:45:03 -0400

    Some integration test scene placeholders.

---
+++ Assets/Tests/Scripts/RosterManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RosterManagerTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //var heroGO = GameObject.FindGameObjectWithTag("Hero");
+        //var roster = heroGO.GetComponent<RosterManager>();
+
+        // Add to roster test.
+
+
+        // Remove from roster test. 
+
+        // Add to allies test.
+
+        // Remove from allies test.
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
