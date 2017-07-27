2897787f48e55328ff7e1696a0593ae3898680ef
Marc King <mjking@umich.edu>
2017-07-27 09:24:16 -0400

    Added display of unlocked and assigned abilities to SceneInfo.

--- Assets/Tests/Scripts/SceneInfo.cs
+++ Assets/Tests/Scripts/SceneInfo.cs
@@ -10,6 +10,8 @@ public class SceneInfo : MonoBehaviour
 
     public bool showSceneTime = true;
     public bool showExperience = false;
+    public bool showUnlockedAbilities = false;
+    public bool showAssignedAbilities = false;
 	
 	void Update ()
     {
@@ -18,6 +20,8 @@ public class SceneInfo : MonoBehaviour
 
         if (showSceneTime) frameText.AppendLine("Time: " + Time.timeSinceLevelLoad);
         if (showExperience) frameText.AppendLine("Experience: " + GameManager.HeroManager.experience);
+        if (showUnlockedAbilities) frameText.AppendLine("Unlocked Abilities: " + GameManager.AbilityManager.unlockedAbilities.ToDelimitedString());
+        if (showAssignedAbilities) frameText.AppendLine("Assigned Abilities: " + GameManager.AbilityManager.assignedAbilities.ToDelimitedString());
 
         displayText.text = frameText.ToString();
 	}
