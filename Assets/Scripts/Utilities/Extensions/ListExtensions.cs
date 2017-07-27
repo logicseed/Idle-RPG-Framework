494b34d4c964ce7191e7fe256100851b95d28052
Marc King <mjking@umich.edu>
2017-07-27 09:23:43 -0400

    Added generic list extension to turn a list of strings into a delimited string.

---
+++ Assets/Scripts/Utilities/Extensions/ListExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public static class ListExtensions
+{
+    public static string ToDelimitedString(this List<string> list, string delimiter = ", ")
+    {
+        return string.Join(delimiter, list.ToArray());
+    }
+}
